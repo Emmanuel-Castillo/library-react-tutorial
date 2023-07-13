94f57f4e73772a682d41eea973318bb4cc569653
Emmanuel <emmanuel.2001alonso.ec@gmail.com>
2023-07-12 22:35:56 -0500

    library-react-tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Nav />
+          <Routes>
+
+          <Route path="/" element={<Home/>}/>
+          <Route path="/books" element={<Books books={books}/>} />
+          <Route path="/books/1" element={() => <BookInfo books={books}/>} />
+          </Routes>
+          
+          
+          <Footer />
+      </div>
+    </Router>
   );
 }
 
